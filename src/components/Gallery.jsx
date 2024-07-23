789b1de9133ad583db62b8c112647aad8aeadfe5
cem-insharo <cem.bugey@insharo.consulting>
2024-07-23 20:25:24 +0700

    UI improvement

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import ImgContainer from "./ImgContainer";
+import CircularProgress from '@mui/material/CircularProgress';
 
 
 const Gallery = () => {
 
     const [images, setImages] = useState([]);
     console.log("images:", images);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
         const fetchImages = async () => {
+            // setLoading(true);
+            setError(null);
             try {
                 const response = await fetch(`/api/fetchImages`);
                 console.log("useEffect fetchImages response: ", response);
@@ -22,7 +27,10 @@ const Gallery = () => {
                 console.log("useEffect fetchImages data: ", data);
                 setImages(data);
             } catch (error) {
+                setError('Failed to fetch images');
                 console.error('Failed to fetch images:', error);
+            } finally {
+                setLoading(false);
             }
         };
         fetchImages();
@@ -31,10 +39,18 @@ const Gallery = () => {
         return () => clearInterval(intervalId); // Clean up on component unmount
     }, []);
 
-    if (!images || images.length === 0) {
-        return (
-            <h2 className="m-4 text-2xl font-bold">No Images Found</h2>
-        )
+    // if (!images || images.length === 0) {
+    //     return (
+    //         <h2 className="m-4 text-2xl font-bold">No Images Found</h2>
+    //     )
+    // }
+
+     if (loading) {
+        return <div className="flex justify-center items-center h-full"><CircularProgress /></div>;
+    }
+
+    if (error) {
+        return <h2 className="m-4 text-2xl font-bold text-red-500">{error}</h2>;
     }
 
     return (
